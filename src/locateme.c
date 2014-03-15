e4f7f156d9e6c0cbbfa7167f1e8cf573256112e5
Mattias Andrée <maandree@operamail.com>
2014-03-15 02:16:11 +0100

    guess by timezone
    Signed-off-by: Mattias Andrée <maandree@operamail.com>

--- src/locateme.c
+++ src/locateme.c
@@ -17,11 +17,38 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <stdio.h>
+#include <time.h>
 
 
 int main(int argc, char** argv)
 {
-  printf("%f %f\n", 0.f, 0.f);
+  /* Guess (poorly) by timezone */
+  time_t time_utc;
+  time_t time_local;
+  time_t time_diff;
+  struct tm* time_local_;
+  float longitude;
+  
+  time_utc = time(NULL);
+  time_local_ = localtime(&time_utc);
+  time_local = time_local_->tm_hour;
+  time_local = time_local * 60 + time_local_->tm_min;
+  time_local = time_local * 60 + time_local_->tm_sec;
+  if (time_local_->tm_isdst > 0)
+    time_local -= 60 * 60; /* nullify summer time adjustment */
+  
+  time_utc %= 24 * 60 * 60;
+  time_diff = time_local - time_utc;
+  time_diff = (time_diff + 30) / 60; /* minutes */
+  time_diff = (time_diff + 15) / 30; /* half hours */
+  
+  longitude = (float)time_diff * 180.f / 12.f / 2.f;
+  if (longitude > 180.f)
+    longitude -= 360.f;
+  if (longitude < -180.f)
+    longitude += 360.f;
+  
+  printf("0.0 %f\n", longitude);
   
   return 0;
 }
