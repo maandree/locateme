fb37e1d7c32a71f7d4554a39128b35ca42ae2f3f
Mattias Andrée <maandree@kth.se>
2016-04-20 14:10:33 +0200

    Read location from ~/.config/geolocation and /etc/geolocation
    Signed-off-by: Mattias Andrée <maandree@kth.se>

--- src/locateme.c
+++ src/locateme.c
@@ -19,6 +19,8 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <pwd.h>
 
 #include "coordinator.h"
 #include "conffile.h"
@@ -39,6 +41,8 @@ int main(int argc, char** argv)
   FILE* f;
   conffile_t* data;
   conffile_t* data_;
+  char* env;
+  struct passwd* pw;
   
   for (i = 0; i < argc; i++)
     {
@@ -53,6 +57,11 @@ int main(int argc, char** argv)
       
       #undef __
     }
+  
+  if ((env = getenv("HOME")) && *env && !chdir(env));
+  else if ((pw = getpwuid(getuid())) && pw->pw_dir)
+    if (chdir(pw->pw_dir))
+      fprintf(stderr, "%s: cannot change directory to your home directory", *argv);
   
 retry:
   init_coordination(continuous, quickly);
@@ -69,24 +78,36 @@ retry:
       
       data_ = data = malloc(3 * sizeof(conffile_t));
       
-      #define __use(INDEX, METHOD)							\
+      #define __use1(INDEX, METHOD)							\
 	({										\
 	  data[INDEX].argc = 1;								\
 	  data[INDEX].argv = malloc(sizeof(char*));					\
 	  data[INDEX].argv[0] = malloc(strlen(METHOD ".") * sizeof(char));		\
 	  memcpy(data[INDEX].argv[0], METHOD, strlen(METHOD ".") * sizeof(char));	\
 	})
+      #define __use2(INDEX, METHOD, ARGV1)						\
+	({										\
+	  data[INDEX].argc = 2;								\
+	  data[INDEX].argv = malloc(2 * sizeof(char*));					\
+	  data[INDEX].argv[0] = malloc(strlen(METHOD ".") * sizeof(char));		\
+	  memcpy(data[INDEX].argv[0], METHOD, strlen(METHOD ".") * sizeof(char));	\
+	  data[INDEX].argv[1] = malloc(strlen(METHOD ".") * sizeof(char));		\
+	  memcpy(data[INDEX].argv[1], ARGV1, strlen(ARGV1 ".") * sizeof(char));		\
+	})
       
-      __use(0, "cache");
-      __use(1, "timezone-offset");
+      __use1(0, "cache");
+      __use2(1, "read", ".config/geolocation");
+      __use2(2, "read", "/etc/geolocation");
+      __use1(3, "timezone-offset");
       
-      #undef __use
+      #undef __use1
+      #undef __use2
       
-      data[2].argc = -1;
-      data[2].argv = NULL;
+      data[4].argc = -1;
+      data[4].argv = NULL;
     }
   for (; data->argc > 0; data++)
-      run(data->argc, data->argv);
+    run(data->argc, data->argv);
   free(data_);
   
   if (already_done())
